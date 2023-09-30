db513c8a6926b4b18ab977c3daf14aa6fcc657dd
jo13han <97030788+jo13han@users.noreply.github.com>
2023-09-30 22:34:13 +0530

    Add files via upload

---
+++ Cards.jsx
@@ -0,0 +1,273 @@
+import React from "react";
+const Cards = () => {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="p-2 mt-4 ml-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail1.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo1.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+                Nepal vs Mongolia | Men's Cricket | Highlights...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Sony LIV</p>
+            <p className="text-[#919191] line-clamp-2">
+              1.5M views <b>.</b> 11 hours ago
+            </p>
+          </div>
+        </div>
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail2.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo2.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+                Riding the Japan's Fastest Bullet Train...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Experience JAPAN</p>
+            <p className="text-[#919191] line-clamp-2">
+              5.5M views <b>.</b> 2 weeks ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail3.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo3.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+                How The World's Largest Paper Company Makes 1/3 of Cardboard{" "}
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Insider Business</p>
+            <p className="text-[#919191] line-clamp-2">
+              3.9M views <b>.</b> 12 days ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail4.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo4.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+                The Fastest Maze-Solving Competition On Earth...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Veritasium</p>
+            <p className="text-[#919191] line-clamp-2">
+              1.5M views <b>.</b> 4 months ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail5.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo5.jpg"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+              JAWAN: Chaleya (Hindi) | Shah Rukh Khan | Nayanthara...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Dip Dey</p>
+            <p className="text-[#919191] line-clamp-2">
+              3.6M views <b>.</b> 2 weeks ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail6.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo6.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+               The Best Scenes from Top Gun 2 : Maverick...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Boxoffice Movie Scenes</p>
+            <p className="text-[#919191] line-clamp-2">
+              13M views <b>.</b> 10 months ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 ml-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail7.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo7.jpg"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+                (PS5) Camouflage Sniping Mission | Realistic Immersive...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Champs Network</p>
+            <p className="text-[#919191] line-clamp-2">
+              5.5M views <b>.</b> 3 months ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail8.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo8.jpg"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+               iPhone 15 Pro Review: The Good, The Bad &...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Marques Brownlee</p>
+            <p className="text-[#919191] line-clamp-2">
+              4.4M views <b>.</b> 1 day ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail9.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo9.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+               Even Billionaires Can't Afford These 10 Cars...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">THE RSG</p>
+            <p className="text-[#919191] line-clamp-2">
+              187K views <b>.</b> 1 month ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail10.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo10.jpg"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+               The Festival of Abundance! | India's Mega...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">National Geographic India</p>
+            <p className="text-[#919191] line-clamp-2">
+             453K views <b>.</b> 6 days ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail11.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo11.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+               Micahel Jackson's Drummer Jonathan Moffett Performs...
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Drumeo</p>
+            <p className="text-[#919191] line-clamp-2">
+              5M views <b>.</b> 6 months ago
+            </p>
+          </div>
+        </div>
+
+        <div className="p-2 mt-4 mr-1 rounded min-w-[300px] w-8">
+          <div>
+            <img
+              src="/src/components/thumbnail12.png"
+              className="h-auto max-w-full rounded"
+            ></img>
+            <div className="flex flex-row space-x-1">
+              <img
+                src="/src/components/logo12.png"
+                className="h-8 w-8 mt-2 rounded-full"
+              ></img>
+              <h1 className="text-m font-bold line-clamp-2 text-white">
+               Mr. Bean Live Performance at the London 2012 Olympic Games..
+              </h1>
+            </div>
+            <p className="text-[#919191] line-clamp-2">Olympics</p>
+            <p className="text-[#919191] line-clamp-2">
+              114M views <b>.</b> 11 years ago
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
---
+++ Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <div className="h-52">
+          <img className="cursor-pointer h-52 max-w-full rounded ml-auto mr-auto mt-4" src="/src/components/heroimg.png"/>
+        </div>
+    </>
+  )
+}
+
+export default Hero
